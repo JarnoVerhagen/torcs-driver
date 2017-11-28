d46e549e7be064f0eceb5e6c07308f4af92759ae
Jarno Verhagen <jarnoverhagen@hotmail.com>
2017-11-28 11:46:46 +0100

    Added logging to manual driver

--- driver/manual_driver.py
+++ driver/manual_driver.py
@@ -3,12 +3,22 @@
 from pytocl.main import main
 from pytocl.driver import Driver
 from pytocl.car import State, Command
+
+
 import keyboard
+import datetime
+import csv
 
 class ManualDriver(Driver):
     accelerator = 0
     brake = 0
     steering = 0
+
+    def __init__(self, logdata=False):
+        date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        file = open('logs/log' + date + '.csv', 'a')
+        self.csvWriter = csv.writer(file, delimiter=',')
+        super(ManualDriver, self).__init__()
 
     def drive(self, carstate: State) -> Command:
         # Accelerator
@@ -37,18 +47,16 @@ class ManualDriver(Driver):
         else:
             # Stop all steering
             self.steering = 0
-	
+
 	# Gear, done using automatic transmission
         gear = max([carstate.gear, 1]) # At least in gear 1
         if self.accelerator > 0 and carstate.rpm > 8000 and gear < 6:
             gear = gear + 1
-            print('up',gear)
-        if carstate.rpm < 2000 and gear > 1:
+        if carstate.rpm < 3000 and gear > 1:
             gear = gear - 1
-            print('down',gear)
         if keyboard.is_pressed('shift'):
             gear = -1
-        
+
         # Create command
         command = Command()
         command.accelerator = self.accelerator
@@ -56,11 +64,8 @@ class ManualDriver(Driver):
         command.gear = gear
         command.steering = self.steering
         command.focus = 0.0
-        
-        # Logging, right now done using provided logger which uses pickle
-        if self.data_logger:
-            self.data_logger.log(carstate, command)
 
+        self.csvWriter.writerow([command.accelerator, command.brake, command.steering, carstate.gear, carstate.speed_x, carstate.speed_y, carstate.speed_z, carstate.distance_from_center, carstate.angle, carstate.rpm, carstate.gear] + list(carstate.distances_from_edge))
         return command
 
 if __name__ == '__main__':
