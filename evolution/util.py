d747fd76565f796abf834cbe148ae19d6162e638
Jarno Verhagen <jarnoverhagen@hotmail.com>
2017-12-08 18:48:20 +0100

    Implemented function for creating new generation based on previous one

--- evolution/util.py
+++ evolution/util.py
@@ -3,10 +3,14 @@ from mutate import mutate
 from driver.driver import Driver
 from driver.main import main
 from xml.dom import minidom
+from operator import itemgetter
+from shutil import copyfile
+import numpy as np
 import tensorflow as tf
 import os
 import threading
 import subprocess
+import csv
 
 def create_initial_model():
     model = Model()
@@ -41,6 +45,51 @@ def create_initial_generation():
     for thread in threads:
         thread.join()
     print("Finished generation of generation 0")
+
+def create_next_generation(parent_generation):
+    parent_folder = 'models/gen' + str(parent_generation) + '/'
+    child_folder = 'models/gen' + str(parent_generation+1) + '/'
+    if not os.path.exists(child_folder):
+        os.makedirs(child_folder)
+
+    # Get list of results from previous generation and sort
+    results_file = open(parent_folder+'results', 'rt')
+    results = list(csv.reader(results_file, delimiter=",", quoting=csv.QUOTE_NONE))
+    del results[0]
+    for i, result in enumerate(results):
+        if float(result[1]) < 40.0:
+            results[i][1] = str(9999.9)
+    results = sorted(results, key=itemgetter(1))
+    results = [result[0] for result in results]
+
+    probabilities = []
+    # Create array of probabilities for each ranking position
+    for i in range(len(results)):
+        probabilities.append(1/(i+1))
+    # Normalize probabilities
+    total_p = sum(probabilities)
+    for i,p in enumerate(probabilities):
+        probabilities[i] = p/total_p
+
+    print("Starting generation of generation " + str(parent_generation+1))
+    # Pick parents which should be kept alive for the next generation
+    parents_to_keep = np.random.choice(results,15,p=probabilities,replace=False)
+    for parent in parents_to_keep:
+        copyfile(parent_folder + parent, child_folder + parent)
+
+    mean = 0
+    std = 0.005
+    # Pick parents which get a child through mutation
+    parents_to_mutate = np.random.choice(results,35,p=probabilities,replace=True)
+    threads = []
+    for i in range(len(parents_to_mutate)):
+        model_name = "g" + str(parent_generation+1) + "m" + str(i)
+        threads.append(threading.Thread(target=mutate, args=(parent_folder + parents_to_mutate[i], child_folder + model_name, mean, std)))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    print("Finished generation of generation " + str(parent_generation+1))
 
 def drive_model(path):
     model = tf.keras.models.load_model(path)
@@ -106,4 +155,4 @@ def test_generation(generation):
 
 if __name__ == '__main__':
     # grade_model('models/gen0/g0m10')
-    test_generation(0)
+    create_next_generation(0)
